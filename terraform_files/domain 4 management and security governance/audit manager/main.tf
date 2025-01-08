f28eaf5af3613e8c36c8b17212bf0f0df17864f2
Mr. Ak <aqshey@gmail.com>
2025-01-08 13:05:28 +0100

    reformatting

--- terraform_files/domain 4 management and security governance/audit manager/main.tf
+++ terraform_files/domain 4 management and security governance/audit manager/main.tf
@@ -1,9 +1,12 @@
 // create audit manager
+provider "aws" {
+  region = "eu-central-1"
+}
 
 resource "aws_auditmanager_assessment" "assessment" {
-  name = "my-assessment"
+  name                    = "my-assessment"
   assessment_framework_id = aws_auditmanager_framework.framework.id
-  framework_id = ""
+  framework_id            = ""
   roles {}
 }
 
